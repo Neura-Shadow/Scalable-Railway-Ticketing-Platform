7144892096742d4300a4ac9899a835d29548ab54
Neura-Shadow <Lingjoker0625@gmail.com>
2026-07-23 13:40:15 +0800

    fix: align admission readiness with schema v7

--- cmd/admission-worker/main.go
+++ cmd/admission-worker/main.go
@@ -25,7 +25,7 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-const admissionSchemaVersion = 6
+const admissionSchemaVersion = 7
 
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
